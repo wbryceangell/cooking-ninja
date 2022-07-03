f92021e68b718a60515d976743a558a8972ebeb0
William Bryce Angell <wbryceangell@gmail.com>
2022-07-03 09:02:15 -0500

    fetch recipe on recipe page

--- src/pages/recipe/Recipe.tsx
+++ src/pages/recipe/Recipe.tsx
@@ -1,9 +1,24 @@
+import { useParams } from "react-router-dom";
+import useFetch from "../../hooks/useFetch";
+import { RecipeData } from "../../types";
 import "./Recipe.css";
 
-interface Props {}
+type Props = {};
 
 const Recipe: React.FC<Props> = ({}) => {
-  return <div>Recipe</div>;
+  const { id } = useParams<{ id: string }>();
+  const {
+    data: recipe,
+    isPending,
+    error,
+  } = useFetch<RecipeData>(`http://localhost:3001/recipes/${id}`);
+  return (
+    <div className="recipe">
+      {error && <p className="error">{error.message}</p>}
+      {isPending && <p className="loading">Loading...</p>}
+      {recipe && <h1>{recipe.title}</h1>}
+    </div>
+  );
 };
 
 export default Recipe;
