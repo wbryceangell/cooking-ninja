ae6d70afbed197285a815ced208e9c29e68259ed
William Bryce Angell <wbryceangell@gmail.com>
2022-07-03 14:09:42 -0500

    initial setup of the create page

--- src/pages/create/Create.tsx
+++ src/pages/create/Create.tsx
@@ -1,9 +1,53 @@
+import { useState } from "react";
 import "./Create.css";
 
-interface Props {}
+type Props = {};
 
 const Create: React.FC<Props> = ({}) => {
-  return <div>Create</div>;
+  const [title, setTitle] = useState("");
+  const [method, setMethod] = useState("");
+  const [cookingTime, setCookingTime] = useState("");
+  const handleSubmit: React.FormEventHandler<HTMLFormElement> = (e) => {
+    e.preventDefault();
+    console.log(title, method, cookingTime);
+  };
+  return (
+    <div className="create">
+      <h2 className="page-title">Add a New Recipe</h2>
+      <form onSubmit={handleSubmit}>
+        <label>
+          <span>Recipe title:</span>
+          <input
+            type="text"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+            required
+          />
+        </label>
+
+        {/* ingredients go here */}
+
+        <label>
+          <span>Recipe method:</span>
+          <textarea
+            value={method}
+            onChange={(e) => setMethod(e.target.value)}
+            required
+          />
+        </label>
+        <label>
+          <span>Cooking time (minutes):</span>
+          <input
+            type="number"
+            value={cookingTime}
+            onChange={(e) => setCookingTime(e.target.value)}
+            required
+          />
+        </label>
+        <button className="btn">Submit</button>
+      </form>
+    </div>
+  );
 };
 
 export default Create;
